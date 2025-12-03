35b4abfb687091651214038fbc698cca0f318ce4
Maks <120607012+magz-27@users.noreply.github.com>
2025-12-03 13:07:23 +0100

    Dodano elementy i jeden LinerarLayout

--- app/src/main/java/com/example/pracanalekcji/MainActivity.java
+++ app/src/main/java/com/example/pracanalekcji/MainActivity.java
@@ -1,6 +1,6 @@
-package com.example.pracanalekcji;
-
-import android.os.Bundle;
+package com.example.pracanalekcji;import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // spinner
+
+        Spinner spinner = findViewById(R.id.spinner2);
+
+        String[] items = {"Kierownik", "Starszy programista", "Młodszy programista", "Tester"};
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                items
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
     }
 }
